39f1f3e196963e0f6ccc381fbf7bdd58dd11ba8a
Melo508 <misicus57@gmail.com>
2026-02-01 20:32:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ORIGENES = [-1001978284886,
     -1003718109552]
 DESTINO = -1003829930906
 
-KEYWORDS = ["Approved.!! ✅",,"Approved! ✅","Approved ✅","APPROVED! ✅"]
+KEYWORDS = ["Approved.!! ✅","Approved! ✅","Approved ✅","APPROVED! ✅"]
 
 client = TelegramClient(StringSession(string_sess), api_id, api_hash)
 
@@ -35,5 +35,6 @@ async def main():
 if __name__ == "__main__":
 
     asyncio.run(main())
+
 
 
